682b516dd71c73cedf5f913880e11c5598836fe0
Jeffrey Chery <jeffchery009@gmail.com>
2025-07-23 05:10:53 -0400

    first commit

---
+++ app/(tabs)/explore.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function TabTwoScreen() {
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText type="title">Explore</ThemedText>
+      <ThemedText>This is the explore tab.</ThemedText>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 16,
+  },
+});
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function HomeScreen() {
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText type="title">Welcome</ThemedText>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
