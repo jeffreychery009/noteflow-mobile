b4d8dfa8f43b516f0ca21c988635b115efaf3d10
Jeffrey Chery <jeffchery009@gmail.com>
2025-07-23 05:17:28 -0400

    Installed new dependecies / ESLint, Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,40 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    plugins: {
+      "tailwindcss": require("eslint-plugin-tailwindcss"),
+    },
+    rules: {
+      semi: ["error"],
+      quotes: ["error", "double"],
+      "prefer-arrow-callback": ["error"],
+      "prefer-template": ["error"],
+      "tailwindcss/classnames-order": ["error"],
+      "tailwindcss/no-custom-classname": "warn",
+      "tailwindcss/no-contradicting-classname": "error",
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling", "index"],
+          ],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
+  {
+    ignores: ["dist/*"],
   },
 ]);
