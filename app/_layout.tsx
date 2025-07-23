c995926aad73fd663ac899da5cbd113efb916fd2
Jeffrey Chery <jeffchery009@gmail.com>
2025-07-23 05:38:58 -0400

    Imported nativewind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
+import "../nativewind-env";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
 
