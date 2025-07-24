2d204d8e686c96c3248351bccb7d87b0cf909eca
Jeffrey Chery <jeffchery009@gmail.com>
2025-07-24 08:28:51 -0400

    Update changes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,13 @@
-import { View } from "react-native";
+import { SafeAreaView, View } from "react-native";
 
 import { ThemedText } from "@/components/ThemedText";
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <ThemedText type="title">Welcome Jeffrey</ThemedText>
-    </View>
+    <SafeAreaView className="flex-1">
+      <View className="flex-1 items-center justify-center bg-gray-100">
+        <ThemedText type="title">Welcome Jeffrey</ThemedText>
+      </View>
+    </SafeAreaView>
   );
 }
